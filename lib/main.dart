2822772586d407a5cf68b9f6c100adbbd4da4400
dyleen <sumayadyleen.com>
2025-08-14 13:54:39 +0800

    feat: add Person

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'car.dart';
+import 'person.dart';
 
 void main() {
   var myCar = Car('Toyota', 'Hilux');
   myCar.showInfo();
+
+  var myPerson = Person (' dyleen', 20);
+  myPerson.sayHello();
+  
 }
